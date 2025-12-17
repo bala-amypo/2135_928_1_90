8a8e6b8a9b822704cc787c2db8582400d4ce5a73
bala-amypo <bala@amypo.in>
2025-12-17 04:55:53 +0000

    User commit

--- src/main/java/com/example/demo/entity/Studententity.java
+++ src/main/java/com/example/demo/entity/Studententity.java
@@ -14,8 +14,12 @@ public class Studententity{
     public void setId(int id){
         this.id=id;
     }
-    public String getName)()
+    public String getName(){
+        return name;
     }
-    }
+    public void setName(String name){
+        this.name = name;
     }
 }
+
+   
---
+++ src/main/java/com/example/demo/service/Studentservice.java
@@ -0,0 +1,8 @@
+package com.example.demo.service;
+import java.util.List;
+import.util.Optional;
+impoirt com.example.demo.entity.Studententity;
+public interface StudentService {
+    Studententity saveData(Studententity st);
+    Studententity insertStudententity(Studententity)
+}
