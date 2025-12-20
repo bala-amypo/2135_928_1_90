2f88dc7e3f7033ce428042d7b0f35c0336cea118
bala-amypo <bala@amypo.in>
2025-12-20 06:48:16 +0000

    User commit

--- src/main/java/com/example/demo/config/SwaggerConfig.java
+++
@@ -1,9 +0,0 @@
-@Configuration
-public class SwaggerConfig {
-    @Bean
-    public OpenAPI openAPI() {
-        return new OpenAPI()
-                .info(new Info().title("Complaint Duplicate Detector")
-                .version("1.0"));
-    }
-}
