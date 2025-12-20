a5f658a71a568391ef65f1ad65f527f8fc9e7bc4
bala-amypo <bala@amypo.in>
2025-12-20 06:24:15 +0000

    User commit

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -1,36 +1,10 @@
 package com.example.demo.controller;
 
 import java.util.List;
-import org.springframework.web.bind.annotation.*;
 
-import com.example.demo.model.TicketCategory;
-import com.example.demo.repository.TicketCategoryRepository;
-
-@RestController
-@RequestMapping("/categories")
-public class TicketCategoryController {
-
-    private final TicketCategoryRepository repo;
-
-    public TicketCategoryController(TicketCategoryRepository repo) {
-        this.repo = repo;
-    }
-
-    @GetMapping
-    public List<TicketCategory> getAll() {
-        return repo.findAll();
-    }
-
-    @PostMapping
-    public TicketCategory save(@RequestBody TicketCategory c) {
-        return repo.save(c);
-    }
-}
-package com.example.demo.controller;
-
-import java.util.List;
-
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/users")
