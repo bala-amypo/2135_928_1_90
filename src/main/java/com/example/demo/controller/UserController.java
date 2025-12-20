1b4e58bfd2e3608d3c339e83b108e5fb5b671f2d
bala-amypo <bala@amypo.in>
2025-12-20 06:42:41 +0000

    User commit

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -2,16 +2,14 @@ package com.example.demo.controller;
 
 import java.util.List;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/users")
 public class UserController {
 
     @GetMapping
-    public List<String> getUsers() {
+    public List<String> users() {
         return List.of("User1", "User2");
     }
 }
