cf94cd17bea85247cea6d7275bcac1af07d1978b
bala-amypo <bala@amypo.in>
2025-12-25 09:06:00 +0000

    User commit

--- src/main/java/com/example/demo/config/SwaggerConfig.java
+++ src/main/java/com/example/demo/config/SwaggerConfig.java
@@ -1,19 +1,16 @@
 package com.example.demo.config;
-
 import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
+import java.util.List;
 @Configuration
 public class SwaggerConfig {
-
-    @Bean
-    public OpenAPI customOpenAPI() {
-        return new OpenAPI()
-                .info(new Info()
-                        .title("Demo Application API")
-                        .version("1.0.0")
-                        .description("API documentation for Demo Spring Boot Application"));
-    }
-}
+@Bean
+public OpenAPI customOpenAPI() {
+    return new OpenAPI()
+    .servers(List.of(
+        new Server().url("https://9329.pro604cr.amypo.ai/")
+        ));
+        }
+        }
