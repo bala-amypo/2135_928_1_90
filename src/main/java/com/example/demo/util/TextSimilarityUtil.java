06b89aebd140efe814275d87b13b03312e5ffe6f
bala-amypo <bala@amypo.in>
2025-12-25 16:42:03 +0000

    User commit

--- src/main/java/com/example/demo/util/TextSimilarityUtil.java
+++ src/main/java/com/example/demo/util/TextSimilarityUtil.java
@@ -9,22 +9,18 @@ public class TextSimilarityUtil {
      String[] words1 = s1.split("\\s+");
      String[] words2 = s2.split("\\s+");
      int matchCount = 0;
-
-        for (String w1 : words1) {
-            for (String w2 : words2) {
-                if (w1.equals(w2)) {
-                    matchCount++;
-                    break;
+     for (String w1 : words1) {
+        for (String w2 : words2) {
+            if (w1.equals(w2)) {
+                matchCount++;
+                break;
                 }
-            }
         }
-
-        int maxLength = Math.max(words1.length, words2.length);
-
-        if (maxLength == 0) {
-            return 0.0;
+     }
+     int maxLength = Math.max(words1.length, words2.length);
+     if (maxLength == 0) {
+        return 0.0;
         }
-
         return (double) matchCount / maxLength;
     }
 }
