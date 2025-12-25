6351e7624331a6b0686ab476c14620a08cacb836
bala-amypo <bala@amypo.in>
2025-12-25 16:27:02 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/DuplicateRuleServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/DuplicateRuleServiceImpl.java
@@ -1,19 +1,13 @@
 package com.example.demo.service.impl;
-
 import com.example.demo.model.DuplicateRule;
 import com.example.demo.repository.DuplicateRuleRepository;
 import com.example.demo.service.DuplicateRuleService;
 import org.springframework.stereotype.Service;
-
 import java.util.List;
-
 @Service
 public class DuplicateRuleServiceImpl implements DuplicateRuleService {
-
     private final DuplicateRuleRepository duplicateRuleRepository;
-
-    public DuplicateRuleServiceImpl(DuplicateRuleRepository duplicateRuleRepository) {
-        this.duplicateRuleRepository = duplicateRuleRepository;
+    public DuplicateRuleServiceImpl(DuplicateRuleRepository duplicateRuleRepository) {this.duplicateRuleRepository = duplicateRuleRepository;
     }
 
     @Override
