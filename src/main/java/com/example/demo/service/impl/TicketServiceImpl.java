6b77d93211a71b0148a61ddc66f1bd63411f69bc
bala-amypo <bala@amypo.in>
2025-12-25 15:57:10 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/TicketServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/TicketServiceImpl.java
@@ -1,17 +1,12 @@
 package com.example.demo.service.impl;
-
 import com.example.demo.model.Ticket;
 import com.example.demo.repository.TicketRepository;
 import com.example.demo.service.TicketService;
 import org.springframework.stereotype.Service;
-
 import java.util.List;
-
 @Service
 public class TicketServiceImpl implements TicketService {
-
     private final TicketRepository ticketRepository;
-
     public TicketServiceImpl(TicketRepository ticketRepository) {
         this.ticketRepository = ticketRepository;
     }
--- src/main/java/com/example/demo/service/impl/UserServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/UserServiceImpl.java
@@ -1,32 +1,24 @@
 package com.example.demo.service.impl;
-
 import com.example.demo.model.User;
 import com.example.demo.repository.UserRepository;
 import com.example.demo.service.UserService;
 import org.springframework.stereotype.Service;
-
 import java.util.List;
-
 @Service
 public class UserServiceImpl implements UserService {
-
     private final UserRepository repository;
-
     public UserServiceImpl(UserRepository repository) {
-        this.repository = repository;
+    this.repository = repository;
     }
-
     @Override
     public User registerUser(User user) {
         return repository.save(user);
     }
-
     @Override
     public User getUser(Long id) {
         return repository.findById(id)
-                .orElseThrow(() -> new RuntimeException("User not found"));
+        .orElseThrow(() -> new RuntimeException("User not found"));
     }
-
     @Override
     public List<User> getAllUsers() {
         return repository.findAll();
