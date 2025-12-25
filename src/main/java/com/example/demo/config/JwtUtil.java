762024cec891e65eeac1c3058271c11d337f4354
bala-amypo <bala@amypo.in>
2025-12-25 09:20:13 +0000

    User commit

--- src/main/java/com/example/demo/config/JwtUtil.java
+++ src/main/java/com/example/demo/config/JwtUtil.java
@@ -13,40 +13,29 @@ public class JwtUtil {
     private final Key key = Keys.secretKeyFor(SignatureAlgorithm.HS256);
         public String generateToken(String username) {
         return Jwts.builder()
-                .setSubject(username)
+        .setSubject(username)
                 .setIssuedAt(new Date(System.currentTimeMillis()))
                 .setExpiration(new Date(System.currentTimeMillis() + JWT_TOKEN_VALIDITY))
                 .signWith(key)
                 .compact();
     }
-
-  
     public String extractUsername(String token) {
         return extractClaim(token, Claims::getSubject);
     }
-
-   
     public Date extractExpiration(String token) {
         return extractClaim(token, Claims::getExpiration);
     }
-
-    
     public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
         final Claims claims = extractAllClaims(token);
         return claimsResolver.apply(claims);
     }
-
-    
     public boolean validateToken(String token, String username) {
         final String extractedUsername = extractUsername(token);
         return extractedUsername.equals(username) && !isTokenExpired(token);
     }
-
-    
     private boolean isTokenExpired(String token) {
         return extractExpiration(token).before(new Date());
     }
-
     private Claims extractAllClaims(String token) {
         return Jwts.parserBuilder()
                 .setSigningKey(key)
