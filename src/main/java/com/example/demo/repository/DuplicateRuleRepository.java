59014a3100aa49535b9c2bd85ebec5fac4e6542f
bala-amypo <bala@amypo.in>
2025-12-25 09:59:14 +0000

    User commit

--- src/main/java/com/example/demo/repository/DuplicateRuleRepository.java
+++ src/main/java/com/example/demo/repository/DuplicateRuleRepository.java
@@ -1,8 +1,6 @@
 package com.example.demo.repository;
-
 import com.example.demo.model.DuplicateRule;
 import org.springframework.data.jpa.repository.JpaRepository;
-
 public interface DuplicateRuleRepository extends JpaRepository<DuplicateRule, Long> {
 
     DuplicateRule findByRuleName(String name);
