6b74578b2ad587a4eec8da1121ecef16451073da
bala-amypo <bala@amypo.in>
2025-12-25 09:41:14 +0000

    User commit

--- src/main/java/com/example/demo/model/Ticket.java
+++ src/main/java/com/example/demo/model/Ticket.java
@@ -25,15 +25,11 @@ public class Ticket {
     public void setUser(User user) { this.user = user; }
     public TicketCategory getCategory() { return category; }
     public void setCategory(TicketCategory category) { this.category = category; }
-
     public String getSubject() { return subject; }
     public void setSubject(String subject) { this.subject = subject; }
-
     public String getDescription() { return description; }
     public void setDescription(String description) { this.description = description; }
-
     public String getStatus() { return status; }
     public void setStatus(String status) { this.status = status; }
-
     public LocalDateTime getCreatedAt() { return createdAt; }
 }
